49aeb117cc84420981611d21bc6d3acf1c052655
Riki225 <79812982+Riki225@users.noreply.github.com>
2021-06-20 16:17:13 +1000

    IntroProgAss1ANSWER

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace liststask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+    string answer;
+    List<int> numList = new List<int>();
+    Console.WriteLine("Rolling a dice...");
+    Random rand1 = new Random();
+    int diceNum1 = rand1.Next(1, 7);
+    numList.Add(diceNum1);
+    Console.WriteLine("You rolled a " + diceNum1);
+    if (diceNum1 < 7)
+    Console.WriteLine("Continue to roll a dice? (y/n)");
+    answer = Console.ReadLine();
+    while (answer != "n")
+    {
+        Random rand2 = new Random();
+        int diceNum2 = rand1.Next(1, 7);
+        Console.WriteLine("You rolled a " + diceNum2);
+        Console.WriteLine("Continue to roll the dice? (y/n)");
+        numList.Add(diceNum2);
+        answer = Console.ReadLine();
+    }
+    Console.WriteLine("Enter amount of rolls to examine:"); 
+    int rolls = int.Parse(Console.ReadLine()); 
+
+    Console.Write("Rolls were: ");
+    int total = 0;
+    for(int i = 0; i < rolls; i++)
+    {
+        Console.Write(numList[i] + " ");
+        total += numList[i];
+    }
+    double avg = (double)total / rolls;
+    double avgc = Math.Round((double)avg, 2);
+    Console.WriteLine("");
+    Console.WriteLine("Total is: " + total);
+    Console.WriteLine("Average is: " + avgc);
+            }
+        }
+    }
+}
